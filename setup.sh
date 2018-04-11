738e0e85ee90a40ec9323b5dbaafb5878c73c46c
Szu-Hsuan, Wu <shuan@csie.io>
2018-04-11 10:21:17 +0800

    Add .gitconfig

--- setup.sh
+++ setup.sh
@@ -2,20 +2,11 @@
 
 cp .tmux.conf ~
 cp .vimrc ~
+cp .gitconfig ~
 cat .bashrc >> ~/.bashrc
 cat .cshrc >> ~/.cshrc
 
 # vim setting
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall
-
-# git
-git config --global core.editor vim     #設定預設編輯器
-git config --global merge.tool vimdiff  #設定預設合併工具
-
-# alias
-git config --global alias.co checkout
-git config --global alias.br branch
-git config --global alias.ci commit
-git config --global alias.st status
 
