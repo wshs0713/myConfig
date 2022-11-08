627402ca5bc6d3f83e19db22d6c6f2149c1cefcf
wshs0713 <shuan@csie.io>
2022-11-08 17:30:42 +0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,17 @@
 # !/bin/bash/
 
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 # Download oh-my-zsh plugin
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
+# Copy config
 cp .tmux.conf ~
 cp .vimrc ~
 cp .gitconfig ~
+cp .ideavimrc ~
+cp -r vimfiles ~
 cat .bashrc >> ~/.bashrc
 cat .cshrc >> ~/.cshrc
 
